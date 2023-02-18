ea2f6246160d79b4c11fd241ce5c96c3b617969d
MBanerjea <mayukh.flip22@gmail.com>
2023-02-19 00:26:26 +0530

    fixes the whole project by including exception handlers for incorrect and no inputs. Also fixes the case where any of r g or b is 0.

--- eas.js
+++ eas.js
@@ -10,43 +10,62 @@ button.textContent = "Enter the number of squares";
 
 button.setAttribute('style', 'margin: 10px auto; padding: 8px; border-radius: 10px; background-color: #f7e2dd;');
 
+const steps = 10;
+
+// First round values values
+let boxes = 16;
+let grid = boxes ** 2;
+const vHeight = (window.innerHeight) - 46; //46 is the top and bottom space required by the button.
+
+const vWidth = window.innerWidth;
+let boxHeight = (vHeight - (boxes * 4)) / boxes;
+let boxWidth = (vWidth - (boxes * 4)) / boxes;
+
+// First round of drawing
+drawGrid(grid, boxHeight, boxWidth);
+
 
 button.addEventListener('click', reDraw);
 
 
-
-let boxes = 16;
-let grid = boxes ** 2;
-const vHeight = (window.innerHeight) - 46;
-const vWidth = window.innerWidth;
-
-let attHeight = (vHeight - (boxes * 4)) / boxes;
-let attWidth = (vWidth - (boxes * 4)) / boxes;
-
-drawGrid(grid, attHeight, attWidth);
-
 function promptMe() {
   do {
-    boxes = prompt("Enter the number of boxes on each side: ");
-  } while (boxes > 100 || boxes < 1);
+    boxes = prompt("Enter the number of boxes between 2 and 100 on each side: ");
+
+    // Check for String input
+    if (Number.isNaN(+boxes)) {
+      alert("Input was not a number. Setting number of boxes to default: 16");
+      boxes = 16;
+
+      // check for no input
+    } else if (!boxes) {
+      alert("No input. Setting number of boxes to default: 16");
+      boxes = 16;
+    }
+
+    // check for input within range
+  } while (boxes > 100 || boxes < 2);
   return boxes;
 }
 
-function drawGrid(grid, attHeight, attWidth) {
+function drawGrid(grid, boxHeight, boxWidth) {
+  // initialising all values for the divs.
   for (let i = 0; i < grid; i++) {
     let div = mainContainer.appendChild(document.createElement('div'));
-    div.classList.add('group256');
+    div.classList.add('boxGroup');
+    // Setting ID isn't required but done for experimental purposes.
     div.setAttribute('id', `n:${i}`);
-    div.setAttribute('style', 'margin: 2px; background-color: rgb(256,256,256);')
+    div.setAttribute('style', 'margin: 2px; background-color: rgb(255,255,255);')
     div.dataset.redFactor = 0;
     div.dataset.greenFactor = 0;
     div.dataset.blueFactor = 0;
-    div.style.height = `${attHeight}px`;
-    div.style.width = `${attWidth}px`
+    div.style.height = `${boxHeight}px`;
+    div.style.width = `${boxWidth}px`
   };
-  const divs = document.querySelectorAll('.group256');
 
+  const divs = document.querySelectorAll('.boxGroup');
 
+  // Decide whether to colour (if white) or shade a div (if already coloured).
   divs.forEach(div => {
     div.addEventListener('mouseover', (e) => {
       if (e.target.style.backgroundColor === "rgb(255, 255, 255)") {
@@ -59,27 +78,29 @@ function drawGrid(grid, attHeight, attWidth) {
 }
 
 function removeGrid() {
-  let grid = (document.querySelectorAll(".group256")).length;
+  // Get the total number of divs to be removed.
+  let grid = (document.querySelectorAll(".boxGroup")).length;
   for (let i = 0; i < grid; i++) {
-    mainContainer.removeChild(document.querySelector(".group256"));
+    mainContainer.removeChild(document.querySelector(".boxGroup"));
   }
+}
+
+function reDraw() {
+  promptMe();
+  removeGrid();
+  grid = boxes ** 2;
+  let boxHeight = (vHeight - (boxes * 4)) / boxes;
+  let boxWidth = (vWidth - (boxes * 4)) / boxes;
+  drawGrid(grid, boxHeight, boxWidth);
 }
 
 function colorGenerator() {
   let color = "#";
+  // Convert output of random to hexadecimal and take 6 characters after the 0. .
   color += Math.random().toString(16).slice(2, 8);
   return color;
 }
 
-
-function reDraw(grid) {
-  promptMe();
-  removeGrid();
-  grid = boxes ** 2;
-  let attHeight = (vHeight - (boxes * 4)) / boxes;
-  let attWidth = (vWidth - (boxes * 4)) / boxes;
-  drawGrid(grid, attHeight, attWidth);
-}
 
 function shader(currentColor, div) {
   let rgbString = currentColor.slice(4, -1);
@@ -88,27 +109,23 @@ function shader(currentColor, div) {
   let green = rgbArray[1];
   let blue = rgbArray[2];
 
-  // alert(div.dataset.redFactor);
-  // alert(typeof (div.dataset.redFactor));
-
-  if (div.dataset.redFactor != 0) {
-    currentColor = newColor(red, green, blue, div);
-    div.style.backgroundColor = `rgb(${currentColor})`;
-  } else {
+  if (div.dataset.redFactor == 0 && div.dataset.greenFactor == 0 && div.dataset.blueFactor == 0) {
     setFactor(red, green, blue, div);
-    currentColor = newColor(red, green, blue, div);
-    div.style.backgroundColor = `rgb(${currentColor})`;
   }
+
+  currentColor = newColor(red, green, blue, div);
+  div.style.backgroundColor = `rgb(${currentColor})`;
 }
 
-
+// since we need to darken a colour by 10%, the factor is value/10.
+// if colour is already black, then darkening factor should be 0.
 function setFactor(r, g, b, div) {
-  div.dataset.redFactor = (r !== 0) ? Math.ceil(r / 10) : 0;
-  div.dataset.greenFactor = (g !== 0) ? Math.ceil(g / 10) : 0;
-  div.dataset.blueFactor = (b !== 0) ? Math.ceil(b / 10) : 0;
+  div.dataset.redFactor = (r !== 0) ? Math.ceil(r / steps) : 0;
+  div.dataset.greenFactor = (g !== 0) ? Math.ceil(g / steps) : 0;
+  div.dataset.blueFactor = (b !== 0) ? Math.ceil(b / steps) : 0;
 }
 
-
+// subtract the SAME factor from the shaded colour every time. Thus achieving rgb( 0, 0, 0) in 10 
 function newColor(r, g, b, div) {
   let newRed = r - (div.dataset.redFactor);
   let newGreen = g - (div.dataset.greenFactor);
